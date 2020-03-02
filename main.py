4e1c99c45ad33baabd401b5c0076248e1d42cd98
BPRO <iserver12345@gmail.com>
2020-03-02 22:41:15 +0500

    Make some changes

--- main.py
+++ main.py
@@ -61,4 +61,5 @@ def webhook():
     return "!", 200
 
 
-server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
